16d43bd2addf7f870c2715cb8a1254ceddf50984
gtracey <gtracey@gmail.com>
2021-12-11 18:52:23 +0000

    Tour page and contact page now added

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+alert("linked");
+function userName() {
+
+
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+
+  var email = document.getElementById("email").value;
+
+
+  alert(fname +" " +lname+ " Thank you for your details. \n" + " We Will be in touch with you via :" + email);
+  //reference to the hide function that renders the form element as hidden
+  // hide();
 
 
 
 
+
+}//function 1
